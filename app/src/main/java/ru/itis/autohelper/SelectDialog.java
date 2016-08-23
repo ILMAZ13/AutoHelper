64842017b48e4a1bddf4e86139a0220e3b270b3d
Hlopunov_Anatoliy <hlopunov598@gmail.com>
2016-08-24 01:31:01 +0300

    Add class SelectDialog

---
+++ app/src/main/java/ru/itis/autohelper/SelectDialog.java
@@ -0,0 +1,25 @@
+package ru.itis.autohelper;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Admin on 23.08.2016.
+ */
+public class SelectDialog extends DialogFragment {
+    private String detail_name;
+    private String km;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+
+
+        builder.setTitle("").setTitle(R.)
+    }
+}
